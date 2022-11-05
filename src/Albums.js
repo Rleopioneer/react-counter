467ea6e971c598f92e8cb951554edb0eb055dffa
Ramon Leonardo <ramon.c.leo@hotmail.com>
2022-11-05 10:49:10 -0300

    adiciona requsição para uma API e consome os dados

--- src/Albums.js
+++ src/Albums.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Albums = () => {
 
+    const [loading, setLoading] = useState(false)
+
+    const [albums, setAlbums] = useState([])
+
+    //Evita que a requisição seja feita toda vez que o componente for renderizado ou atualizado.
+    useEffect(() => {
+        setLoading(true)
+
+        fetch('https://jsonplaceholder.typicode.com/albums').then(res => res.json()).then(data => {
+            console.log(data)
+            setAlbums(data)
+            setLoading(false)
+        })
+    }, [])
+    
+
     return (
-        <h1>Álbuns</h1>
+        <> 
+            <h1>Álbuns</h1>
+
+            {
+                loading === true ? 'Carregando...' : ''
+            }
+
+            {
+                albums.map(album => {
+                    return (
+                        <div>
+                            <span> {album.title} - id: {album.id} </span>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 
 }
--- src/Users.js
+++ src/Users.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 const Users = () => {
+    const [loading, setLoading] = useState(false)
+    
+    //faz requisição
+    useEffect(() => {
+        setLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/users').then(res =>res.json()).then(data => {
+            
+            setUsers(data)
+            setLoading(false)
+            
+        })
+    }, [])
+
+    //exibe dados da requisição
+    const [users, setUsers] = useState([])
 
     return (
-        <h1>Users</h1>
+        <>
+            <h1>Users</h1>
+            {
+                loading === true ? 'Carregando ...' : ''
+            }
+
+            {
+                users.map(user => {
+                    return (
+                        <div style={ {margin:'10px'} }>
+                            <span> {user.id} - {user.name} - Nome de usuário: {user.username} - E-mail: {user.email}</span>
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 
 }
